96816cae017d052c2984f52f9a59527ce041a881
Exodus <Relzarick@gmail.com>
2025-03-22 21:36:21 +0800

    finished most css layout

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 
 // t https://pokeapi-proxy.freecodecamp.rocks/api/pokemon
 // t https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/{name-or-id}
+
+const blueBtnContainer = document.querySelector(".blue-btn-container");
+
+for (let i = 1; i <= 10; i++) {
+  const btn = document.createElement("div");
+  btn.classList.add("blue-btn");
+  blueBtnContainer.appendChild(btn);
+}
